b7eb1851d57a90eb42d23070aeffc33eac384870
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-05 18:41:50 +0300

    - add

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,10 +1,24 @@
 import React, {useState, useEffect} from 'react';
+import {Link} from "react-router-dom";
 
 const BlogPage = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(data => setPosts(data))
+    },[])
+
     return (
         <div>
-            <h1>Blog page</h1>
-            <p>This is a demo website about react router dom</p>        </div>
+            <h1>Our news</h1>
+            {posts.map(post => (
+                <Link key={post.id} to={`/posts/${post.id}`}>
+                    <li>{post.title}</li>
+                </Link>
+            ))}
+            </div>
     );
 };
 
--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 
 const SinglePage = () => {
     const {id} = useParams()
+    const [post, setPost] = useState(null)
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(res => res.json())
+            .then(data => setPost(data))
+    }, [id])
 
     return (
         <div>
-            {id}
+            {post && (
+                <>
+                    <h1>{post.title}</h1>
+                    <p>{post.body}</p>
+                </>
+            )}
         </div>
     );
 };
