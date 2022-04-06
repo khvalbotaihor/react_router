ad77b7ad786889efca7d1dd6dd23ea2cdeebb43f
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-06 14:35:42 +0300

    - add mock auth

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,9 +1,19 @@
 import React, {useState, useEffect, } from 'react';
-import {Link, useLocation} from "react-router-dom";
+import {Link, useLocation, useSearchParams} from "react-router-dom";
 
 const BlogPage = () => {
     const [posts, setPosts] = useState([]);
-    console.log(useLocation())
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const postQuery = searchParams.get('post') || '';
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target
+
+        const query = form.search.value
+        setSearchParams({post: query})
+    }
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -14,8 +24,14 @@ const BlogPage = () => {
     return (
         <div>
             <h1>Our news</h1>
+            <form autoComplete="off" onSubmit={handleSubmit}>
+                <input type="search" name="search"/>
+                <input type="submit" value='Search'/>
+            </form>
             <Link to='/posts/new'>Add New Post</Link>
-            {posts.map(post => (
+            {posts.filter(
+                post => post.title.includes(postQuery)
+            ).map(post => (
                 <Link key={post.id} to={`/posts/${post.id}`}>
                     <li>{post.title}</li>
                 </Link>
